b3b1512aa106d4ad3d5c06107f7d6ce6995a28ad
merlinpushpa <65481140+merlinpushpa@users.noreply.github.com>
2020-05-17 17:53:28 +0530

    Create guessinggame.sh

---
+++ guessinggame.sh
@@ -0,0 +1,29 @@
+function game()
+{
+while true
+do
+echo "enter your guess:"
+read value
+if [[$value =~ [^0-9]]
+then
+echo "$value is not an integer"
+echo "enter a integer value:"
+elif [[$value -eq $total]]
+then
+echo "Congratualtion! your guess is correct"
+break
+elif [[$value -gt $total]]
+then
+echo "$value is too high"
+else
+echo "$value is too low"
+fi
+done
+}
+echo "start the game"
+echo "guess the number of files in current directory"
+total=$(ls -l|grep "^-"|wc -l)
+value =- 1
+game
+echo "game ended"
+
